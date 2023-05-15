525126058170ec796bf8456669563f6c70284057
Michael Edelman <michael@edel.monster>
2023-05-14 22:27:11 -0400

    dockerize project

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
+import prefetch from "@astrojs/prefetch";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://edelorg.com",
-	integrations: [react(), sitemap(), mdx(), tailwind()],
-	output: 'server',
-	adapter: vercel()
+  site: "https://edelorg.com",
+  integrations: [react(), sitemap(), mdx(), tailwind(), partytown(), compress(), prefetch()],
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
