0f9193872b222117b0eaab365e5980a0afe50c6b
Michael Edelman <michael.edelman@marriott-sp.com>
2023-05-15 16:27:22 -0400

    fix: tweak intra repo deps

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,30 +1,22 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
-import sitemap from '@astrojs/sitemap'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import partytown from '@astrojs/partytown'
-import compress from 'astro-compress'
-import prefetch from '@astrojs/prefetch'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import compress from 'astro-compress';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://edelorg.com',
-  markdown: {
-    shikiConfig: {
-      theme: 'material-theme-palenight'
-    }
-  },
-  integrations: [
-    react(),
-    sitemap(),
-    mdx(),
-    tailwind(),
-    partytown(),
-    compress(),
-    prefetch()
-  ],
-  output: 'server',
-  adapter: vercel()
-})
+	site: 'https://edelorg.com',
+	markdown: {
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+		},
+	},
+	integrations: [react(), mdx(), tailwind()],
+	output: 'server',
+	adapter: vercel(),
+});
